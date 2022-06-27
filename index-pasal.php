fba99a183e17bf6c583b5d2217624c541347640c
Shodik12 <67968446+Shodik12@users.noreply.github.com>
2022-06-27 11:24:05 +0700

    Add files via upload

---
+++ index-pasal.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My Project!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+  </head>
+  <body>
+  <section class="section">
+    <div class="container">
+      
+       
+      <div class="container-fluid">
+      <div class="card-content">
+      <div class="media">
+      <div class="media-right">
+      <style>
+      .gambar{
+      height: 100px;
+      width: 120px;
+      }
+      </style>
+         
+      <img src="https://i.ibb.co/S3wmKJ7/Logo-Bank-Perkreditan-Rakyat-laluahmad-com.png" class="gambar" alt="Placeholder image">
+      
+      </div>
+      
+      </div>
+      
+      <p class="subtitle">
+        My <strong>Project</strong>!
+      </p>
+      
+    </div>
+  </section>
+  
+  
+  <div class="notification is-link is-light">
+  <button class="delete"></button>
+  Example Search:Pasal 1 ..</div>
+  <article class="panel is-link">
+  <p class="panel-heading">
+  Search Pasal
+  </p>
+ <form action="index-pasal.php" method="post">
+  <div class="panel-block">
+  <p class="control has-icons-left">
+  <input class="input is-link" type="text" name="search" id="search" placeholder="Search Pasal..">
+  <span class="icon is-left">
+ <svg id="file-search" data-name="multi color" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon multi-color" width="48" height="48"><path id="tertiary-fill" d="M4,3H7A0,0,0,0,1,7,3V20a0,0,0,0,1,0,0H4a1,1,0,0,1-1-1V4A1,1,0,0,1,4,3Z" style="fill: rgb(222, 202, 202); stroke-width: 2;"></path><path id="primary-stroke" d="M8,20H4a1,1,0,0,1-1-1V4A1,1,0,0,1,4,3h8.59a1,1,0,0,1,.7.29l3.42,3.42a1,1,0,0,1,.29.7V8" style="fill: none; stroke: rgb(18, 46, 226); stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path><path id="secondary-stroke" d="M10,15a4,4,0,1,1,4,4A4,4,0,0,1,10,15Zm11,6-4-3.4" style="fill: none; stroke: rgb(237, 231, 250); stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path></svg>  </span>
+  </p>
+  <div class="buttons">
+  <button class="button is-link" type="submit">Search</button>
+   </div>
+  </div>
+</form>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script type="text/javascript">
+ document.addEventListener('DOMContentLoaded', () => {
+ (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+ const $notification = $delete.parentNode;
+ 
+ $delete.addEventListener('click', () => {
+ $notification.parentNode.removeChild($notification);
+ });
+ });
+ });
+ 
+  
+  </script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-expander/1.7.0/jquery.expander.js"></script>
+  <script type="text/javascript">
+  $(document).ready(function () {
+  $(".readmore").expander({
+  slicePoint : 130,
+  expandText: 'Lihat lebih banyak...',
+  userCollapseText : 'Lihat lebih sedikit...'
+  });
+  });
+  </script>
+  </article>
+  
+  
+  
+  
+  
+ <table class="table table-striped table-bordered">
+ <thead>
+ <tr>
+ <th width="1%">No</th>
+ <th>Pasal</th>
+ 
+ 
+ </tr>
+ </thead>
+ <tbody>
+ <?php
+ $no = 1;
+ $koneksi = mysqli_connect("sql209.hstn.me", "mseet_31970282", "Anda1234", "mseet_31970282_kienweb");
+ $batas = 5;
+ $halaman = isset($_GET['halaman'])?(int)$_GET['halaman'] : 1;
+ $halaman_awal = ($halaman>1) ? ($halaman * $batas) - $batas : 0;	
+ 
+ $previous = $halaman - 1;
+ $next = $halaman + 1;
+ 
+ $data = mysqli_query($koneksi,"select * from siswa");
+ $jumlah_data = mysqli_num_rows($data);
+ $total_halaman = ceil($jumlah_data / $batas);
+ 
+ $keyword="";
+ if (isset($_POST['search'])) {
+ $keyword = $_POST['search'];
+ }
+ $id = $_GET['id'];
+ $query = mysqli_query($koneksi,"SELECT * FROM siswa WHERE pasal LIKE '%".$keyword."%' OR ".$no." LIKE '%".$keyword."%'  ORDER BY id DESC Limit $halaman_awal,$batas");
+ $hitung_data = mysqli_num_rows($query);
+ if ($hitung_data > 0) {
+ while ($data = mysqli_fetch_array($query)) {
+ ?>
+ <tr>
+ <td><?php echo $no++; ?></td>
+ 
+ <td class="readmore"><?php  echo $data['pasal'];?></td>
+ <td class="align-center"><a class="button is-light" href="hapus.php?id=<?php echo $data['id'];?>">hapus</a></td>
+ 
+ </tr>
+ <?php } } else { ?> 
+ <tr>
+ <td colspan='4' class="text-center">Tidak ada data ditemukan</td>
+ </tr>
+ <?php } ?>
+ </tbody>
+ 
+ 
+ </table>
+  
+  <nav class="pagination is-right" role="navigation" aria-label="pagination">
+  <a class="pagination-previous" <?php if($halaman > 1){ echo "href='?halaman=$Previous'"; } ?>>Previous</a>
+  <a class="pagination-next" <?php if($halaman < $total_halaman) { echo "href='?halaman=$next'"; } ?>>Next page</a>
+  <ul class="pagination-list">
+<?php 
+ for($x=1;$x<=$total_halaman;$x++){
+ ?> 
+ <li><a class="pagination-link" href="?halaman=<?php echo $x ?>"><?php echo $x; ?></a></li>
+ <?php
+ }
+ ?>		
+     </ul>
+  </nav>
+  
+  </div>
+  </body>
+  
+  <footer class="footer">
+  <div class="content has-text-centered">
+  <p>
+  <p class="subtitle">
+        PT. Bank<strong><br>BPR CIREBON</strong>
+      </p>
+    </p>
+  </div>
+  </footer>
+</html>
---
+++ post-bab.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Tinymce Editor in using PHP Example - Nicesnippets.com</title>
+    <!-- include Boostrap 5 -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" />
+    <!-- include tinymce -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.6.0/umd/popper.min.js" integrity="sha512-BmM0/BQlqh02wuK5Gz9yrbe7VyIVwOzD1o40yi1IsTjriX/NGF37NyXHfmFzIlMmoSIBXgqDiG1VNU6kB5dBbA==" crossorigin="anonymous"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    
+    <script src="https://cdn.tiny.cloud/1/pwc2pk462mtevom6738o53c0gtnelt66owbf6dp7u9gde99z/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+</head>
+<body>
+    <?php 
+        $koneksi = mysqli_connect("sql209.hstn.me", "mseet_31970282", "Anda1234", "mseet_31970282_kienweb");
+        
+
+        if(isset($_POST['submit'])){
+              $nama_bab = $_POST['nama_bab'];
+
+            if($nama_bab != ''){
+                mysqli_query($koneksi, "INSERT INTO siswa(nama_bab) VALUES('".$nama_bab."') ");                
+                   header('location: index-bab.php?id=sukses');                              
+                   
+            }
+        }
+    ?>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header text-center bg-primary text-white">
+                        <h4>Form Post Project(Bab)</h4>
+                    </div>
+                    <div class="card-body">
+                        <form method="post" action="post-bab.php">
+                            
+                            <div class="mb-1">
+                                <label><strong>Isi disini :</strong></label>
+                                <textarea id="mytextarea" name="nama_bab" class="form-control"></textarea><br>
+                            </div>
+                            <div class="d-flex justify-content-center">
+                                <input type="submit" name="submit" value="Submit" class="btn btn-success">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Script -->
+    <script>
+        tinymce.init({
+            selector: '#mytextarea',
+            plugins: [
+                'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
+                'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
+                'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
+            ],
+            toolbar: 'undo redo | formatpainter casechange styleselect | bold italic backcolor | ' +
+            'alignleft aligncenter alignright alignjustify | ' +
+            'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
+        });
+    </script>
+</body>
+</html>
