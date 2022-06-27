0be13637936dd329249e302fbf6bb93d75aca302
Shodik12 <67968446+Shodik12@users.noreply.github.com>
2022-06-27 11:29:36 +0700

    Update hapus.php

--- hapus.php
+++ hapus.php
@@ -3,5 +3,5 @@ $koneksi = mysqli_connect("sql209.hstn.me", "mseet_31970282", "Anda1234", "mseet
 $id = $_GET['id'];
 mysqli_query($koneksi,"DELETE FROM siswa WHERE id='$id'")or die(mysql_error());
  
-header("location:index.php?pesan=hapus");
+header("location:index-pasal.php?pesan=sukses");
 ?>
